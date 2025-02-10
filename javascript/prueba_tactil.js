1c61220cea5910b8c7afef961f691b2d62c68dd6
jesusmuve18 <jesumuve19@gmail.com>
2025-02-10 17:10:43 +0100

    Panel tactil

--- javascript/prueba_tactil.js
+++ javascript/prueba_tactil.js
@@ -3,14 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let touchTimeout;
     let inputRange;
+    const TIME = 1000;
 
-    document.addEventListener("touchstart", (event) => {
-        if (event.touches.length !== 1) return; // Solo un dedo
-
-        touchTimeout = setTimeout(() => {
-            if (!inputRange) {
-                // Crear el input range solo si no existe
-                inputRange = document.createElement("input");
+    inputRange = document.createElement("input");
                 inputRange.type = "range";
                 inputRange.min = 0;
                 inputRange.max = 100;
@@ -20,15 +15,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 inputRange.style.top = "80%";
                 inputRange.style.width = "200px";
                 inputRange.style.transform = "translateX(-50%)";
+                inputRange.classList.add("hidden");
 
-                document.body.appendChild(inputRange);
-            }
-        }, 500); // Mantener presionado por 500ms
+    document.body.appendChild(inputRange);
+
+    document.addEventListener("touchstart", (event) => {
+        if (event.touches.length !== 1) return; // Solo un dedo
+
+        touchTimeout = setTimeout(() => {
+            inputRange.classList.remove("hidden");
+        }, TIME); // Mantener presionado por 500ms
+
+        
     });
 
     document.addEventListener("touchmove", (event) => {
         if (!inputRange) return;
-        inputRange.classList.remove("hidden");
+        
         const touch = event.touches[0];
         let newValue = 50 + ((touch.pageX - inputRange.offsetLeft) / inputRange.offsetWidth) * 100;
         inputRange.value = Math.max(0, Math.min(100, newValue));
